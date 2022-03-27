e1ec5144214628a25bba89a97324d924c207f315
velikrgl <velikrgl@yahoo.com>
2022-03-27 22:41:05 +0300

    database con for connection tab

--- includes/pages/connection.php
+++ includes/pages/connection.php
@@ -1,40 +1,77 @@
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      Connection
+    </h1>
+  </section> <!-- Main content -->
 
-    <section class="content-header">
-      <h1>
-       Conecction
-      </h1>
-    </section>    <!-- Main content -->
+
+  <section class="content">
+
+    <!-- Default box -->
+    <div class="box">
+      <div class="box-header with-border">
+        <table class="table table-striped">
+          <thead style="background-color: black; color: white; " class="thead-dark">
+            <tr>
+              <th scope="col">id</th>
+              <th scope="col">Connection Name</th>
+              <th scope="col">Status</th>
+              <th scope="col">BlackOrWhite</th>
+              <th scope="col">CreatedUser</th>
+              <th scope="col">Creation Date</th>
+            </tr>
+          </thead>
+
+          <?php
+
+          $conn = new mysqli('localhost','root','','gradproj');
+          $sql = "SELECT * FROM  connections";
+          $sql2 = "SELECT * FROM connection_info";
+          $result = $conn->query($sql);
+          $result2 =$conn->query($sql2);
+
+    
+          if ($result->num_rows > 0) {
+
+              while ($rows = $result->fetch_assoc()) {
+          ?>
+                <tr>
+                  <!--FETCHING DATA FROM EACH 
+                    ROW OF EVERY COLUMN-->
+                  <td><?php echo $rows['id']; ?></td>
+                  <td><?php echo $rows['connection_name']; ?></td>
+                  <td><?php echo $rows['status']; ?></td>
+                 
+                </tr>
+          <?php
+              }
+            
+          } else {
+            echo "No Result";
+          }
+
+          ?>
 
 
-    <section class="content">
 
-<!-- Default box -->
-<div class="box">
-  <div class="box-header with-border">
-    <h3 class="box-title">Conecction</h3>
-
-    <div class="box-tools pull-right">
-      <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-              title="Collapse">
-        <i class="fa fa-minus"></i></button>
-      <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-        <i class="fa fa-times"></i></button>
+        </table>
+      </div>
     </div>
-  </div>
-  <div class="box-body">
-   Let's Do It
-  </div>
-  <!-- /.box-body -->
-  <div class="box-footer">
-    Footer
-  </div>
-  <!-- /.box-footer-->
+
+    
+    
+
+
+    <!-- /.box-body -->
+
+
+    <!-- /.box-footer-->
 </div>
 <!-- /.box -->
 
-</section>    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+</section> <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
