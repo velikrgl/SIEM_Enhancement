d7ac52caef234e3f70e530f836639a9ba36db070
Ömer YILDIRIM <flashomer@gmail.com>
2022-04-07 13:08:38 +0300

    add deleteAccount function
    add deleteAccount function

--- admin.php
+++ admin.php
@@ -1,6 +1,17 @@
 <?php
 
 include("includes/sec.php");  
+
+if(isset($_GET['act']) and $_GET['act']== "delete") {
+
+    if($_GET['db']== "accounts" and $_GET['id'] > 0){
+         $query = dbQuery("DELETE FROM accounts where id = '".$_GET['id']."' ");
+         $query = explode(":",$query);
+         die($query[0]);
+    }
+}
+
+
 include("includes/header.php");
 
 
