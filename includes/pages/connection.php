74777c7a026e47d0a23f819d174fef3c5302848c
velikrgl <velikrgl@yahoo.com>
2022-04-20 21:26:05 +0300

    connection db is done

--- includes/pages/connection.php
+++ includes/pages/connection.php
@@ -27,28 +27,49 @@
 
           <?php
 
-          $conn = new mysqli('localhost','root','','gradproj');
+          $conn = new mysqli('localhost', 'root', '', 'gradproj');
           $sql = "SELECT * FROM  connections";
-        
-          $result = $conn->query($sql);
-         
 
-    
+          $result = $conn->query($sql);
+
+
+
           if ($result->num_rows > 0) {
 
-              while ($rows = $result->fetch_assoc()) {
+            while ($rows = $result->fetch_assoc()) {
           ?>
-                <tr>
-                  <!--FETCHING DATA FROM EACH 
+              <tr>
+                <!--FETCHING DATA FROM EACH 
                     ROW OF EVERY COLUMN-->
-                  <td><?php echo $rows['id']; ?></td>
-                  <td><?php echo $rows['connection_name']; ?></td>
-                  <td><?php echo $rows['status']; ?></td>
-                 
-                </tr>
+                <td><?php echo $rows['id']; ?></td>
+                <td><?php echo $rows['connection_name']; ?></td>
+                <?php $status = $rows['status'];
+                if ($status == 1) {
+                ?>
+                  <td style="background-color: greenyellow;"> <?php echo "Active" ?></td>
+                <?php
+                } else { ?>
+                  <td style="background-color: red; color: white;"> <?php echo "NO Active" ?></td>
+                <?php
+                }
+                ?>
+
+                <?php $blcwht = $rows['blackOrWhite'];
+                if ($blcwht == 1) {
+                ?>
+                  <td style="background-color: black; color: white;"> <?php echo "BlackList" ?></td>
+                <?php
+                } else { ?>
+                  <td style="background-color: white; color: black;"> <?php echo "WhiteList" ?></td>
+                <?php
+                }
+                ?>
+                <td><?php echo $rows['userwhocreated']; ?></td>
+                <td><?php echo $rows['createdTime']; ?></td>
+
+              </tr>
           <?php
-              }
-            
+            }
           } else {
             echo "No Result";
           }
@@ -61,8 +82,8 @@
       </div>
     </div>
 
-    
 
-</section> <!-- /.content -->
+
+  </section> <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
