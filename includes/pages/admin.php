fc402c04f5455f19a6b2f1c297a084dca3c50832
velikrgl <velikrgl@yahoo.com>
2022-04-24 02:40:08 +0300

    css fixed

--- includes/pages/admin.php
+++ includes/pages/admin.php
@@ -12,59 +12,68 @@
     <section class="content">
 
 
-     <?php
+      <?php
 
-          $conn = new mysqli('localhost', 'root', '', 'gradproj');
-          $sql = "SELECT * FROM  connections";
+      $conn = new mysqli('localhost', 'root', '', 'gradproj');
+      $sql = "SELECT * FROM  connections";
 
-          $result = $conn->query($sql);
-          if ($result->num_rows > 0) {
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0) {
 
-            while ($rows = $result->fetch_assoc()) {
-          ?>
-      <!-- Default box -->
-      <div class="col-sm-8">
-        <div class="col-sm-10">
-          <div style="padding: 20px; border: 2px solid black;" class="card">
-            <div class="card-body">
-              <h5 class="card-title " style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; font-weight: bold;"><?php echo strtoupper($rows['connection_name']); ?></h5>
-              
+        while ($rows = $result->fetch_assoc()) {
+      ?>
+          <!-- Default box -->
+          <div class="col-sm-6">
+            <div class="col-sm-16">
+              <div style="padding: 30px; border: 2px solid black;" class="card">
+                <div class="card-body">
+                  <h5 class="card-title " style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; font-size: larger; font-weight: bold;"><?php echo strtoupper($rows['connection_name']); ?> </h5>
+
+                </div>
+                <div  class="connection_details">
+                  <?php $status = $rows['status'];
+                  if ($status == 1) {
+                  ?>
+                    <i style="color: green; font-weight: bold;" class="fa-solid fa-link fa-xl">
+                      <p style="font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; margin-left: 40px;">Connected</p>
+                    </i>
+                  <?php
+                  } else { ?>
+                    <i style="color: red; font-weight: bold;" class="fa-solid fa-bug-slash fa-xl">
+                      <p style="font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;   margin-left: 40px;">Disconnect</p>
+                      <div style="width: 100px;" >
+                        
+                      </div>
+                    </i>
+
+                  <?php
+                  }
+                  ?>
+                </div>
+
+              </div>
             </div>
-            <div class="connection_details">
-            <?php $status = $rows['status'];
-                if ($status == 1) {
-                ?>
-                  <i style="color: green; font-weight: bold;" class="fa-solid fa-link fa-xl" > <p style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; margin-left: 40px;">Connected</p> </i>
-                <?php
-                } else { ?>
-                 <i style="color: red; font-weight: bold;" class="fa-solid fa-bug-slash fa-xl"><p style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; margin-left: 40px;">Disconnect</p></i>
-                
-                <?php
-                }
-                ?>
-            </div>
+
 
           </div>
-        </div>
-        
-        
-      </div>
-<?php }}?>
+      <?php }
+      } ?>
       <div style="margin-top: 30px;" class="clas">
 
       </div>
 
       <style>
-        .col-sm-6{
+        .col-sm-6 {
           margin-top: 10px;
         }
-        .content{
+
+        .content {
           margin-top: 50px;
           width: 1500px;
         }
       </style>
-      
 
-  </section> <!-- /.content -->
+
+    </section> <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
