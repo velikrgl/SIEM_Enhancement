e48755eb1762a6e1e03707e1fc25721ecd25635c
velikrgl <velikrgl@yahoo.com>
2022-05-14 19:13:07 +0300

    dashboard line graph

--- includes/pages/admin.php
+++ includes/pages/admin.php
@@ -55,11 +55,11 @@
 
             $rows = dbQueryList($sql)->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
             $rows2 = dbQueryList($sql2)->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
-          
-            $total = $rows[0]+$rows2[0];
-            
+
+            $total = $rows[0] + $rows2[0];
+
             ?>
-            <h3><?php echo floor(($rows[0]/$total)*100) ?><sup style="font-size: 20px">%</sup></h3>
+            <h3><?php echo floor(($rows[0] / $total) * 100) ?><sup style="font-size: 20px">%</sup></h3>
 
             <p>Percentage of black list in all data </p>
           </div>
@@ -113,76 +113,157 @@
       <!-- ./col -->
     </div>
 
-    <div class="col-md-6">
-      <!-- LINE CHART -->
-      <div class="box box-info">
-        <div class="box-header with-border">
-          <h3 class="box-title">Line Chart</h3>
+    <!DOCTYPE html>
+    <html lang="en">
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-
-          </div>
-        </div>
-
-
-        <div class="box-body chart-responsive">
-          <div class="chart" id="line-chart" style="height: 300px;"></div>
-
-        </div>
-        <!-- /.box-body -->
-      </div>
-      <!-- /.box -->
-    </div>
-
-    
-
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load("current", {
-        packages: ["corechart"]
-      });
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Language', 'Speakers (in millions)'],
-          <?php 
-         $sql = "SELECT COUNT(id) FROM scanned_db where  scanned_type=1 ;";
-         
-         $rows = dbQueryList($sql)->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
-         $ip = $rows[0];
-
-         $sql2 = "SELECT COUNT(id) FROM scanned_db where  scanned_type=2 ;";
-         $rows2 =dbQueryList($sql2)->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
-         $hash = $rows2[0];
-
-         $sql3 = "SELECT COUNT(id) FROM scanned_db where  scanned_type=3 ;";
-         $rows3 = dbQueryList($sql3)->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
-         $url = $rows3[0];
-          ?>
-          ['IP', <?php echo $ip ?>],
-          ['HASH', <?php echo $hash ?>],
-          ['URL', <?php echo $url ?>],
-          
-        ]);
-
-        var options = {
-          legend: 'none',
-          pieSliceText: 'label',
-          title: 'Scanned Data Distribution',
-          pieStartAngle: 100,
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-        chart.draw(data, options);
-      }
-    </script>
+    </head>
 
     <body>
-      <div id="piechart" style="display: flex; float: right; width: 50%; height: 350px;"></div>
-    </body>
+      <div class="row">
+
+        <div class="col-md-6">
+          <!-- Bar chart -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <i class="fa fa-bar-chart-o"></i>
+
+              <h3 class="box-title">Bar Chart</h3>
+
+            </div>
+            <div class="box-body">
+              <canvas id="myChart" style="height :250px;"></canvas>
+            </div>
+            <!-- /.box-body-->
+          </div>
+          <!-- /.box -->
+        </div>
+
+        <div class="col-md-6">
+
+          <body>
+            <div id="piechart" style="display: flex; float: right; width: 100%; height: 340px;"></div>
+          </body>
+
+          
+        </div>
+
+      </div>
+    
+      <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+          <script type="text/javascript">
+            google.charts.load("current", {
+              packages: ["corechart"]
+            });
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+              var data = google.visualization.arrayToDataTable([
+                ['Data', 'Scanned'],
+                <?php
+                $sql = "SELECT COUNT(id) FROM scanned_db where  scanned_type=1 ;";
+
+                $rows = dbQueryList($sql)->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
+                $ip = $rows[0];
+
+                $sql2 = "SELECT COUNT(id) FROM scanned_db where  scanned_type=2 ;";
+                $rows2 = dbQueryList($sql2)->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
+                $hash = $rows2[0];
+
+                $sql3 = "SELECT COUNT(id) FROM scanned_db where  scanned_type=3 ;";
+                $rows3 = dbQueryList($sql3)->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
+                $url = $rows3[0];
+                ?>['IP', <?php echo $ip ?>],
+                ['HASH', <?php echo $hash ?>],
+                ['URL', <?php echo $url ?>],
+
+              ]);
+
+              var options = {
+                legend: 'none',
+                pieSliceText: 'label',
+                title: 'Scanned Data Distribution',
+                pieStartAngle: 100,
+              };
+
+              var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+              chart.draw(data, options);
+            }
+          </script>
+
+
+    <script>
+
+let myChart = document.getElementById('myChart').getContext('2d');
+
+// Global Options
+Chart.defaults.global.defaultFontFamily = 'Lato';
+Chart.defaults.global.defaultFontSize = 10;
+Chart.defaults.global.defaultFontColor = '#777';
+
+let massPopChart = new Chart(myChart, {
+  type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June','July','August', 'September','October','November','December'],
+    datasets: [{
+      label: 'Data Entries',
+      data: [
+        617594,
+        181045,
+        153060,
+        106519,
+        105162,
+        95072,
+        617594,
+        181045,
+        153060,
+        106519,
+        105162,
+        95072
+      ],
+      backgroundColor:'green',
+      
+      borderWidth: 1,
+      borderColor: '#777',
+      hoverBorderWidth: 3,
+      hoverBorderColor: '#000'
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Data Entries by Month',
+      fontSize: 12
+    },
+    legend: {
+      display: true,
+      position: 'right',
+      labels: {
+        fontColor: '#000'
+      }
+    },
+    layout: {
+      padding: {
+        left: 50,
+        right: 0,
+        bottom: 0,
+        top: 0
+      }
+    },
+    tooltips: {
+      enabled: true
+    }
+  }
+});
+    </script>
+
+
   </section>
   <!-- /.content -->
 
