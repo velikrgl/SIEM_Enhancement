a13a0d05d4dc746bc8dccfaf389d57e8022a5c62
Ömer YILDIRIM <flashomer@gmail.com>
2022-05-15 15:50:29 +0300

    Check URIData Function and getDataURL

--- includes/function.php
+++ includes/function.php
@@ -31,5 +31,32 @@ function dbQueryList($q){
      return $sql;
 }
 
+function uriDataChech($uriData){
+     if(filter_var($uriData, FILTER_VALIDATE_IP)){
+        //ip
+        return 1;
+     } else {
+         if(filter_var($uriData, FILTER_VALIDATE_URL) or strpos($uriData, ".") !== false){
+             //url
+             return 3;
+             } else {
+              //hash
+              return 2;
+             }
+     }
+ }
+
+
+ function getDataUrl($url){
+     $curl_handle=curl_init();
+     curl_setopt($curl_handle, CURLOPT_URL,$url);
+     curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+     curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+     curl_setopt($curl_handle, CURLOPT_USERAGENT, 'AYBU-Graduation Project');
+     $query = curl_exec($curl_handle);
+     curl_close($curl_handle);
+     return $query;
+ }
+
 ?>
  
